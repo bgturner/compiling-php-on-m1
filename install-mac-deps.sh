b92faf12fc76526be0514e1ea19a07ce3759426d
Benjamin Turner <ben.turner@wpengine.com>
2022-08-03 11:53:53 -0700

    feat: create bash script to help install macos deps

---
+++ install-mac-deps.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+
+# Xcode is required by Homebrew
+echo "Checking Xcode installation location:"
+xcode-select -p || xcode-select --install
+
+# Apple Silicon defaults to /opt/homebrew
+#   See: https://docs.brew.sh/Installation
+homebrew_m1_location="/opt/homebrew"
+[ ! -d $homebrew_m1_location ] && \
+    echo "Homebrew installation not found in $homebrew_m1_location" \
+    bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+
+echo "Installing dependencies with Homebrew"
+# TODO: Install compiling deps
+brew install \
+     gcc \
+     libdv \
+     autoconf \
+     bison \
+     re2c \
+     make
+
+
