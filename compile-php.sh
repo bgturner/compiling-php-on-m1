80f19d48bd9e0da97cfcd7f3a3970854a5ee3a4d
Benjamin Turner <ben.turner@wpengine.com>
2022-08-04 10:28:20 -0700

    feat: enable json
    - https://www.php.net/manual/en/book.json.php
    
    > json – Used for communications with other servers and processing data in JSON format.
    > - https://make.wordpress.org/hosting/handbook/server-environment/#php-extensions

--- compile-php.sh
+++ compile-php.sh
@@ -17,6 +17,7 @@ mkdir -p $BUILD_DIR && cd $BUILD_DIR
     --enable-fileinfo \
     --enable-filter \
     --enable-dom \
+    --enable-json \
     --enable-option-checking=fatal \
     --with-curl=$(brew --prefix curl) \
     --with-sodium \
