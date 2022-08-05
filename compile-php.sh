8805f69702b93210e449d5eaaaf35bff576297e8
Benjamin Turner <ben.turner@wpengine.com>
2022-08-05 11:34:46 -0700

    feat: add icu4c to PKG_CONFIG_PATH

--- compile-php.sh
+++ compile-php.sh
@@ -6,7 +6,8 @@ export PATH="/opt/homebrew/opt/bison/bin:$PATH"
 # add openssl
 openssl_path=$(brew --prefix openssl@1.1)
 oniguruma_path=$(brew --prefix oniguruma)
-export PKG_CONFIG_PATH="$PKG_CONFIG_PATH:${oniguruma_path}/lib/pkgconfig:${openssl_path}/lib/pkgconfig"
+icu_path=$(brew --prefix icu4c)
+export PKG_CONFIG_PATH="$PKG_CONFIG_PATH:${oniguruma_path}/lib/pkgconfig:${openssl_path}/lib/pkgconfig:${icu_path}/lib/pkgconfig"
 
 if [ "$BUILD_DIR" == "" ]; then
     BUILD_DIR="$(pwd)/php-7.4.30"
